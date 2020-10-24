3a8ce6b37154ffdc22ef4b4aab5d0f8223e83070
Saveriu Battesti <saveriubattesti@gmail.com>
2020-10-24 11:34:26 +0200

    Partie 8

---
+++ eureka-server/src/main/java/controller/WebController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class WebController implements ErrorController {
+    @RequestMapping("/error")
+    @ResponseBody
+    public String handleError(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+        return String.format("<html><body><h2>Error Page : La page que vous recherchez n'existe pas </h2><div>Status code: <b>%s</b></div>"
+                        + "<div>Exception Message: <b>%s</b></div><body></html>",
+                statusCode, exception==null? "N/A": exception.getMessage());
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "La page que vous recherchez n'existe pas";
+    }
+}
