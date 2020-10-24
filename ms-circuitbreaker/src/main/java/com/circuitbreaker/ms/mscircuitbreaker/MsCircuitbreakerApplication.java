7398c13df517eff02b456799efd0f9c00e98c38e
Saveriu Battesti <saveriubattesti@gmail.com>
2020-10-24 11:47:48 +0200

    final commit

--- ms-circuitbreaker/src/main/java/com/circuitbreaker/ms/mscircuitbreaker/MsCircuitbreakerApplication.java
+++ ms-circuitbreaker/src/main/java/com/circuitbreaker/ms/mscircuitbreaker/MsCircuitbreakerApplication.java
@@ -3,11 +3,13 @@ package com.circuitbreaker.ms.mscircuitbreaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableEurekaClient
 public class MsCircuitbreakerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -6,6 +6,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+//@EnableEurekaClient
+// J'ai mis cette annotation en commentaire parce qu'il semble que la dépendance fait
+// crasher l'application, ça semble fonctionner une fois, puis ça crashe. J'ai pas eu le
+// temps de trouver d'où ça vient.
 public class MicrocommerceApplication {
 
 	public static void main(String[] args) {
