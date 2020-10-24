d2d681d813ef482e9bc884a861bd10057cbf1c39
Saveriu Battesti <saveriubattesti@gmail.com>
2020-10-24 10:53:13 +0200

    fix fallback

--- ms-circuitbreaker/src/main/java/com/circuitbreaker/ms/mscircuitbreaker/MsCircuitbreakerApplication.java
+++ ms-circuitbreaker/src/main/java/com/circuitbreaker/ms/mscircuitbreaker/MsCircuitbreakerApplication.java
@@ -2,8 +2,12 @@ package com.circuitbreaker.ms.mscircuitbreaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MsCircuitbreakerApplication {
 
 	public static void main(String[] args) {
